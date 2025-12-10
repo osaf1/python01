0a6d1d9fea8b5be654cd93d31010b1b92a3888c5
osaf1 <zsafi073@gmail.com>
2025-12-10 14:26:09 +0100

    Add my code

---
+++ feest.py
@@ -0,0 +1,31 @@
+vragen = [
+  "Wat is je voornaam?",
+  "Wat is je achternaam?",
+  "Wat neem je mee aan drank?",
+  "Wat neem je mee aan eten?"
+
+]
+
+
+
+persoon = {
+  "voornaam": "",
+  "achternaam": "",
+  "drank": "",
+  "eten": "",
+}
+
+persoon["voornaam"] = input("1. Wat is je voornaam?\n")
+persoon["achternaam"] = input("2. wat is je achternaam?\n")
+persoon["drank"] = input("3. wat neem je mee aan drank?\n")
+persoon["achternaam"] = input("4. wat neem je aan eten?\n")
+print("Bedankt voor het invullen!")
+print("see you at the party\n")
+with open("feestgangers.txt", "a") as f:
+    f.write("----\n")
+    f.write(f"voornaam: {persoon['voornaam']}\n")
+    f.write(f"achternaam: {persoon['achternaam']}\n")
+    f.write(f"drank: {persoon['drank']}\n")
+    f.write(f"eten: {persoon['eten']}\n")
+
+
